c620e96827f6be224a3db81578ef767a9605bec7
Yousef Alam <flamingspaz96@googlemail.com>
2016-10-04 13:36:00 +0100

    Bump Terraform version to 0.7.4

--- script.sh
+++ script.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-wget https://releases.hashicorp.com/terraform/0.6.16/terraform_0.6.16_linux_amd64.zip -O /tmp/terraform.zip
+wget https://releases.hashicorp.com/terraform/0.7.4/terraform_0.7.4_linux_amd64.zip -O /tmp/terraform.zip
 mkdir /tmp/terraform
 unzip /tmp/terraform.zip -d /tmp/terraform
 chmod 755 -R /tmp/terraform
 mv /tmp/terraform/* /usr/local/bin/
+rm -f /tmp/terraform.zip
 apt-get update
 apt-get install -y unzip python-pip python-dev python-virtualenv libffi-dev libssl-dev
 pip install --upgrade cffi ansible boto six markupsafe

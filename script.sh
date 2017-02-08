22577494a4602f601516cdaeaa242e805b643007
Yousef Alam <flamingspaz96@googlemail.com>
2017-02-08 10:49:31 +0000

    Remove Mesos deb after installing

--- script.sh
+++ script.sh
@@ -12,3 +12,4 @@ pip install --upgrade cffi ansible boto six markupsafe
 
 wget http://repos.mesosphere.com/debian/pool/main/m/mesos/mesos_1.1.0-2.0.107.debian81_amd64.deb -O /tmp/mesos.deb
 dpkg -i /tmp/mesos.deb
+rm -f /tmp/mesos.deb

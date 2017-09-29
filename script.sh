b2dfff514f77e3702066919071dd190b2500d82d
Yousef Alam <flamingspaz96@googlemail.com>
2017-09-29 14:24:04 +0100

    Update mesos to 1.4.0

--- script.sh
+++ script.sh
@@ -10,7 +10,7 @@ apt-get update
 apt-get install -y unzip python-pip python-dev python-virtualenv libffi-dev libcurl4-nss-dev libssl-dev apt-transport-https ca-certificates libevent-dev libsasl2-modules netcat-openbsd
 pip install --upgrade cffi ansible boto six markupsafe pycrypto
 
-wget http://repos.mesosphere.com/debian/pool/main/m/mesos/mesos_1.3.0-2.0.3.debian8_amd64.deb -O /tmp/mesos.deb
+wget http://repos.mesosphere.com/debian/pool/main/m/mesos/mesos_1.4.0-2.0.1.debian8_amd64.deb -O /tmp/mesos.deb
 wget http://ftp.debian.org/debian/pool/main/o/openssl/libssl1.0.0_1.0.1t-1+deb8u6_amd64.deb -O /tmp/libssl1.0.0.deb
 dpkg -i /tmp/libssl1.0.0.deb
 dpkg -i /tmp/mesos.deb

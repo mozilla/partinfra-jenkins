3a538f1882d2221a350871ce9896a0cc9f953eaa
Yousef Alam <flamingspaz96@googlemail.com>
2017-12-01 16:15:30 +0000

    Update Terraform to 0.11.1

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://releases.hashicorp.com/terraform/0.10.6/terraform_0.10.6_linux_amd64.zip -O /tmp/terraform.zip
+wget https://releases.hashicorp.com/terraform/0.11.1/terraform_0.11.1_linux_amd64.zip -O /tmp/terraform.zip
 mkdir /tmp/terraform
 unzip /tmp/terraform.zip -d /tmp/terraform
 chmod 755 -R /tmp/terraform
